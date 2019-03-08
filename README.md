9bac64435a99a1acf0e073a7693ca6492e87a6e9
Andrew Evans <andrew.evans@rhythmandbinary.com>
2019-03-07 21:02:11 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Storybook Post on Rhythm and Binary
+- This project showcases using Storybook with a React and Angular app
+- The apps are named after my cats Chessie and Reyrey
+- Read the blog post here for more info
+
+![alt text]
+
+![Chessie](/chessie)
---
+++ reyrey/src/index.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+class LoginControl extends React.Component {
+    constructor(props) {
+      super(props);
+      this.handleLoginClick = this.handleLoginClick.bind(this);
+      this.handleLogoutClick = this.handleLogoutClick.bind(this);
+      this.state = {isLoggedIn: false};
+    }
+   
+    handleLoginClick() {
+      this.setState({isLoggedIn: true});
+    }
+   
+    handleLogoutClick() {
+      this.setState({isLoggedIn: false});
+    }
+   
+    render() {
+      const isLoggedIn = this.state.isLoggedIn;
+      let button;
+   
+      if (isLoggedIn) {
+        button = <LogoutButton onClick={this.handleLogoutClick} />;
+      } else {
+        button = <LoginButton onClick={this.handleLoginClick} />;
+      }
+   
+      return (
+        <div>
+   
+          {button}
+        </div>
+      );
+    }
+  }
+   
+  function LoginButton(props) {
+    return (
+        <div className="container">
+            <button onClick={props.onClick}>show message</button>
+        </div>
+    );
+  }
+   
+  function LogoutButton(props) {
+    return (
+        <div className="container">
+            <h1>Reyrey likes using Storybook!</h1>
+            <img src={require('./Reyrey.jpg')} alt="Reyrey"></img>
+        </div>
+    );
+  }
+   
+  ReactDOM.render(
+    <LoginControl />,
+    document.getElementById('root')
+  );
